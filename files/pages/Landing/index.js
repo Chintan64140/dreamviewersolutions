01a7881aba86f67fe26dfb0f4a9733fa5a01a7be
Chintan64140 <chintan6414@gmail.com>
2025-11-15 10:29:03 +0530

    Update index.js

--- files/pages/Landing/index.js
+++ files/pages/Landing/index.js
@@ -2,12 +2,7 @@
 import { Box, Stack } from "@mui/material";
 import React from "react";
 import Header from "../../Components/Header";
-import { AuroraBackground } from "@/components/ui/aurora-background";
-import { motion } from "motion/react";
-import WorldMap from "@/components/ui/world-map";
-import Earth from "@/components/ui/earth";
-import { cn } from "@/lib/utils";
-import { TracingBeam } from "@/components/ui/tracing-beam";
+
 import Section1 from "./components/section-1";
 import Section2 from "./components/section-2";
 import Section3 from "./components/section-3";
